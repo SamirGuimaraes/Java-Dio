7c7a989a890e91dee212fde1c66e73f291af770a
Samir Guimarães <samir0312@outlook.com.br>
2024-09-03 21:13:33 -0300

    Exercícios de estruturas de repetição

--- ExercEstruturaFluxo/Exerc06.java
+++ ExercEstruturaFluxo/Exerc06.java
@@ -1,5 +1,6 @@
 /*
- * 
+ * Informar todos os números de 1000 a 1999 que
+ * quando divididos por 11 obtemos resto = 5.
  * 
  */
 
@@ -8,6 +9,12 @@ package ExercEstruturaFluxo;
 public class Exerc06 {
     public static void main(String[] args) {
         
+        for (int i = 1000; i <= 1999; i++){
+            if (i%11 == 5){
+                System.out.println(i);
+            }
+
+        }
 
     }
 }
--- ExercEstruturaFluxo/Exerc07.java
+++ ExercEstruturaFluxo/Exerc07.java
@@ -1,13 +1,48 @@
 /*
+ * Colocar estas duas linhas iniciais no método Main():
+ * - Random rnd = new Random(); //Inicia Aleatório
+ * - int x = rnd.nextInt(101); //Gera um número aleatório (0 – 100)
  * 
+ * 1. Faça um laço de repetição que solicite ao usuário digitar um número;
+ * 2. O laço encerra quando o usuário acertar o número;
+ * 3. Se o número do usuário for menor que o oculto,
+ * escrever: “MAIOR”, se o número do usuário for maior que
+ * o oculto, escrever: “MENOR”.
  * 
  */
 
 package ExercEstruturaFluxo;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Exerc07 {
     public static void main(String[] args) {
-        
+
+        Random rnd = new Random();
+        int numAleatorio = rnd.nextInt(101); 
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Um número de 0 até 100 foi gerado aleatório pelo computado.");
+        System.out.println("Teste sua sorte e escolha um número de 0 a 100: ");
+        int numEscolhido = sc.nextInt();
+
+        while(numEscolhido != numAleatorio){
+            if (numEscolhido > numAleatorio){
+                System.out.println("\nSeu número escolhido é maior que o gerado pelo computador.");
+                System.out.println("Teste novamente sua sorte e escolha um número de 0 a 100: ");
+                numEscolhido = sc.nextInt();
+
+            }else{
+                System.out.println("\nSeu número escolhido é menor que o gerado pelo computador.");
+                System.out.println("Teste novamente sua sorte e escolha um número de 0 a 100: ");
+                numEscolhido = sc.nextInt();
+            }
+
+        }
+        sc.close();
+
+        System.out.println("Parabéns! Você e o computador escolher o número: " +numEscolhido);
 
     }
 }
